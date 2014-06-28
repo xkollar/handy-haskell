9384c5c605c5347f546467ab4ebdf03b14c1fef7
Matej Kollar <208115@mail.muni.cz>
2014-06-28 14:35:54 +0200

    Couple more HLint rules

--- hlint/HLint.hs
+++ hlint/HLint.hs
@@ -3,8 +3,10 @@ import "hint" HLint.Default
 import "hint" HLint.Dollar
 
 import Data.Map
+import Control.Arrow
 
 warn = (\ x -> f $ g x) ==> f . g
+
 -- List
 warn = cycle [c] ==> repeat c
 
@@ -20,4 +22,27 @@ warn = map snd (Data.Map.toList x) ==> Data.Map.elems x
 
 warn = foldr f v (Data.Map.elems x) ==> Data.Map.foldr f v x
 warn = Data.Maybe.fromMaybe d (Data.Map.lookup k m) ==> Data.Map.findWithDefault d k m
+
+-- Arrows
+
+warn = id *** id ==> id
+warn = Control.Arrow.first id ==> id
+warn = Control.Arrow.second id ==> id
+
+warn = Control.Arrow.first f (Control.Arrow.second g x) ==> (f Control.Arrow.*** g) x
+warn = Control.Arrow.second f (Control.Arrow.first g x) ==> (g Control.Arrow.*** f) x
+
+warn = Control.Arrow.first f (Control.Arrow.first g x) ==> Control.Arrow.first (f . g) x
+warn = Control.Arrow.second f (Control.Arrow.second g x) ==> Control.Arrow.second (f . g) x
+
+warn = (a *** b) ((c &&& d) x) ==> ((a . c) Control.Arrow.&&& (b . d)) x
+warn = (a *** b) ((c *** d) x) ==> ((a . c) Control.Arrow.*** (b . d)) x
+
+warn = Control.Arrow.first f ((a *** b) x) ==> ((f . a) Control.Arrow.*** b) x
+warn = Control.Arrow.second f ((a *** b) x) ==> (a Control.Arrow.*** (f . b)) x
+warn = (a *** b) (Control.Arrow.first f x) ==> ((a . f) Control.Arrow.*** b) x
+warn = (a *** b) (Control.Arrow.second f x) ==> (a Control.Arrow.*** (b . f)) x
+
+warn = Control.Arrow.first f ((a &&& b) x) ==> ((f . a) Control.Arrow.&&& b) x
+warn = Control.Arrow.second f ((a &&& b) x) ==> (a Control.Arrow.&&& (f . b)) x
 
