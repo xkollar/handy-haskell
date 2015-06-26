192b1e325a2595c46207616503de6a3f28800aee
Matej Kollar <208115@mail.muni.cz>
2015-06-26 13:41:22 +0200

    Move functions to prepared module

--- handy/src/Handy/Function.hs
+++ handy/src/Handy/Function.hs
@@ -1,1 +1,14 @@
 module Handy.Function where
+
+import Control.Arrow
+
+fixEq :: Eq a => (a -> a) -> a -> a
+fixEq f = fun where
+    fun x = if x == x' then x else fun x' where
+        x' = f x
+
+anotatel :: (a -> b) -> a -> (b, a)
+anotatel = (&&& id)
+
+anotater :: (a -> b) -> a -> (a, b)
+anotater = (id &&&)
