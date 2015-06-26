e18f7d66e0af002a56745d5b80f3f36bff576157
Matej Kollar <208115@mail.muni.cz>
2015-06-26 14:55:51 +0200

    Polish

--- handy/src/Handy/Function.hs
+++ handy/src/Handy/Function.hs
@@ -1,14 +1,26 @@
-module Handy.Function where
+module Handy.Function
+    ( fixEq
+    , anotatel
+    , anotater
+    ) where
 
 import Control.Arrow
 
+-- | Will find fixed point of a function from fiven starting point based on 'Eq' instance.
+--
+-- >>> fixEq cos 0
+-- 0.7390851332151607
 fixEq :: Eq a => (a -> a) -> a -> a
 fixEq f = fun where
     fun x = if x == x' then x else fun x' where
         x' = f x
 
+-- | Use funtion to anotate value, left version.
+--
+-- > sort . anontatel read
 anotatel :: (a -> b) -> a -> (b, a)
 anotatel = (&&& id)
 
+-- | Use funtion to anotate value, right version.
 anotater :: (a -> b) -> a -> (a, b)
 anotater = (id &&&)
