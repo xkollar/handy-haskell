85a2a3de2172e9a9f7880da13a0781160838f01b
Matej Kollar <208115@mail.muni.cz>
2015-08-13 18:31:46 +0200

    Polish tests

--- handy/test/HandyTest.hs
+++ handy/test/HandyTest.hs
@@ -15,20 +15,25 @@ main = defaultMain tests
 tests :: [Test]
 tests =
     [ testGroup "Handy.List"
-        [ testCase "Contains first" test_ordElem_1
-        , testCase "Contains middle" test_ordElem_2
-        , testCase "Contains last" test_ordElem_3
-        , testCase "Does not contains before" test_ordElem_4
-        , testCase "Does not contains middle" test_ordElem_5
-        , testCase "Does not contains after" test_ordElem_6
-        , testCase "Contains infinite" test_ordElem_7
-        , testCase "Does not contains infinite" test_ordElem_8
+        [ testGroup "ordElem"
+            [ testCaseSimple "Contains first" test_ordElem_1
+            , testCaseSimple "Contains middle" test_ordElem_2
+            , testCaseSimple "Contains last" test_ordElem_3
+            , testCaseSimple "Does not contain before" test_ordElem_4
+            , testCaseSimple "Does not contain middle" test_ordElem_5
+            , testCaseSimple "Does not contain after" test_ordElem_6
+            , testCaseSimple "Contains infinite" test_ordElem_7
+            , testCaseSimple "Does not contain infinite" test_ordElem_8
+            ]
         ]
     , testGroup "Trivial"
         [ testCase "test" test_trivial_1
         , testProperty "prop" prop_trivial_1
         ]
     ]
+
+testCaseSimple :: AssertionPredicable t => String -> t -> Test
+testCaseSimple msg b = testCase msg (b @? msg)
 
 test_trivial_1 :: Assertion
 test_trivial_1 = () @?= ()
@@ -39,29 +44,29 @@ prop_trivial_1 x = x == x
 holedFiniteList :: [Int]
 holedFiniteList = [100,102..200]
 
-test_ordElem_1 :: Assertion
-test_ordElem_1 = assertBool "begin" $ 100 `ordElem` holedFiniteList where
+test_ordElem_1 :: Bool
+test_ordElem_1 = 100 `ordElem` holedFiniteList where
 
-test_ordElem_2 :: Assertion
-test_ordElem_2 = assertBool "middle" $ 150 `ordElem` holedFiniteList where
+test_ordElem_2 :: Bool
+test_ordElem_2 = 150 `ordElem` holedFiniteList where
 
-test_ordElem_3 :: Assertion
-test_ordElem_3 = assertBool "end" $ 200 `ordElem` holedFiniteList where
+test_ordElem_3 :: Bool
+test_ordElem_3 = 200 `ordElem` holedFiniteList where
 
-test_ordElem_4 :: Assertion
-test_ordElem_4 = assertBool "before" . not $ 1 `ordElem` holedFiniteList where
+test_ordElem_4 :: Bool
+test_ordElem_4 = not $ 1 `ordElem` holedFiniteList where
 
-test_ordElem_5 :: Assertion
-test_ordElem_5 = assertBool "between" . not $ 151 `ordElem` holedFiniteList where
+test_ordElem_5 :: Bool
+test_ordElem_5 = not $ 151 `ordElem` holedFiniteList where
 
-test_ordElem_6 :: Assertion
-test_ordElem_6 = assertBool "after" . not $ 300 `ordElem` holedFiniteList where
+test_ordElem_6 :: Bool
+test_ordElem_6 = not $ 300 `ordElem` holedFiniteList where
 
-holedInfiniteList :: [Int]
+holedInfiniteList :: [Integer]
 holedInfiniteList = [1,3..]
 
-test_ordElem_7 :: Assertion
-test_ordElem_7 = assertBool "infinite contains" $ 1001 `ordElem` holedInfiniteList
+test_ordElem_7 :: Bool
+test_ordElem_7 = 1001 `ordElem` holedInfiniteList
 
-test_ordElem_8 :: Assertion
-test_ordElem_8 = assertBool "infinite does not contain" . not $ 1000 `ordElem` holedInfiniteList
+test_ordElem_8 :: Bool
+test_ordElem_8 = not $ 1000 `ordElem` holedInfiniteList
