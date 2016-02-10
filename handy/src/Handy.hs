5b451213eaa5da2ebcc6e08723a83e1e24e4e014
Matej Kollar <208115@mail.muni.cz>
2016-02-10 11:30:12 +0100

    Added re-exporting module

---
+++ handy/src/Handy.hs
@@ -0,0 +1,14 @@
+-- |
+-- Module:       $HEADER$
+-- Description:  Various handy functions.
+--
+-- Various handy functions. Maybe useful for scavinger hunts?
+module Handy
+    ( module Handy.Function
+    , module Handy.List
+    , module Handy.Number
+    ) where
+
+import Handy.Function
+import Handy.List
+import Handy.Number
