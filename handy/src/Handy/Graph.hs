a6f32acc1e2f7373d4da83096a242c3b570ccb12
Matej Kollar <208115@mail.muni.cz>
2016-12-18 20:32:50 +0100

    Abstract DS that stores nodes to process...

--- handy/src/Handy/Graph.hs
+++ handy/src/Handy/Graph.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 -- |
 -- Module:       $HEADER$
 -- Description:  Simple tools for working with graph-like data structures.
@@ -6,16 +7,32 @@
 module Handy.Graph
     ( Graph(..)
     , bfs
+    , dfs
+    -- * Some graph-like structures
+    , ImplicitGraph(..)
     ) where
 
 import Control.Arrow
+import Data.List (foldl')
 import qualified Data.Set as Set
+
+import Handy.Container
 
 
 -- | Ds @graph v l@ is a reprezentation of a graph with
 -- vertices of type @v@ and edges labelled by @l@.
 class Graph graph where
     neighbours :: graph v l -> v -> [(v, l)]
+
+-- | Various algorithms on graphs in one package.
+grKata :: (Ord v, Graph g, Container c (v,a)) => ((v,a) -> c (v,a)) -> (a -> (v,l) -> a) -> a -> g v l -> v -> [a]
+grKata sing f u gr pt = go (Set.singleton pt) $ sing (pt, u) where
+    go acc c = case get c of
+        Just ((v,a),q) -> a : go acc' q' where
+            nbrs = neighbours gr v
+            acc' = foldr Set.insert acc $ map fst nbrs
+            q' = foldl' (flip insert) q . map (fst &&& f a) $ filter (flip Set.notMember acc . fst) nbrs
+        Nothing -> []
 
 -- Breadth-first search walk through nodes
 --
@@ -33,12 +50,10 @@ class Graph graph where
 -- bfsPaths = bfs (flip ((:) . fst)) []
 -- @
 bfs :: (Ord v, Graph g) => (a -> (v,l) -> a) -> a -> g v l -> v -> [a]
-bfs f u gr pt = go (Set.singleton pt) [(pt, u)] where
-    go acc ((v,a):q) = a : go acc' q' where
-        nbrs = neighbours gr v
-        acc' = foldr Set.insert acc $ map fst nbrs
-        q' = (q++) . map (fst &&& f a) $ filter (flip Set.notMember acc . fst) nbrs
-    go _ [] = []
+bfs = grKata (singleton :: a -> Queue a)
+
+dfs :: (Ord v, Graph g) => (a -> (v,l) -> a) -> a -> g v l -> v -> [a]
+dfs = grKata (singleton :: a -> [a])
 
 -- | Implicit graph (given vertex returns neigbours and labels of edges that
 -- led to those neighbours).
